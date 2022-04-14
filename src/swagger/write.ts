dc13659ecea1d5e81351b9c7c1c64e8689e055ef
Kirill Nikolaev <10702288+nikolaev-k@users.noreply.github.com>
2022-04-14 11:58:19 -0400

    Write swagger to file

--- src/swagger/write.ts
+++ src/swagger/write.ts
@@ -1,30 +1,18 @@
-import { writeFileSync } from 'fs';
+import { writeFileSync, mkdirSync } from 'fs';
 import stringify from 'fast-safe-stringify';
+import { parse } from 'path';
 import { ISwaggerOptions } from '../interfaces';
 import { getSwagger } from './get';
 
 /**
- * Converts title to file-friendly name.
- * For example: "Service Name" will become `openapi/service-name.json`.
- * @param title - Service title.
- */
-const titleToPath = (title: string): string => `openapi/${title.toLowerCase().replace(' ', '-')}.json`;
-
-/**
- * Returns path to file, defaults to `openapi/service-name.json`.
- * @param options - Swagger options.
- * @param path - Path override (optional).
- */
-const getPath = (options: ISwaggerOptions = {}, path?: string): string => path ?? titleToPath(options.title);
-
-/**
  * Write swagger definitions to a file.
  * @param options - Swagger options.
- * @param path - Path to file (optional), defaults to `openapi/service-name.json`
+ * @param path - Path to file (optional), defaults to `./openapi/api.json`
  */
-export const writeSwagger = async (options: ISwaggerOptions = {}, path?: string) => {
+export const writeSwagger = (options: ISwaggerOptions = {}, path: string = './openapi/api.json'): void => {
   const swagger = getSwagger(options);
   const swaggerString = stringify(swagger);
-  const swaggerPath = getPath(options, path);
-  writeFileSync(swaggerPath, swaggerString);
+  mkdirSync(parse(path).dir, { recursive: true });
+  writeFileSync(path, swaggerString);
+  console.log(`Generated swagger doc in ${path}`);
 };
